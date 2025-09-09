2b54407b09edd15278b1253fbbf841cab1699242
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-09 16:55:08 +0800

    Create Days. 002.java
    Hari kedua commit

---
+++ Days. 002.java
@@ -0,0 +1,14 @@
+package Day2.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "samra";
+        int umur = 18;
+        double tinggi = 144.0;
+        System.out.printf("nama saya %s saya berumur %d dan tinggi saya itu %.1f \n" ,nama,umur,tinggi);
+        System.out.println("nama : "+ nama);
+        System.out.print("umur  "+ umur);
+    }
+    
+}
