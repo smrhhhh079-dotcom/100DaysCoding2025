dd791c88790445e59a637d1fff000a5cdd84f5eb
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-10 16:21:00 +0800

    Create Days.003.java
    Hari ke 3 melakukan ngoding

---
+++ Days.003.java
@@ -0,0 +1,16 @@
+package days3.java ;
+
+public class Main {
+    public static void main(String[] args) {
+    // Membuat variabel
+    String nama = "Samra";
+    int umur = 18;
+
+    // Menampilkan output
+    System.out.println("Nama  : " + nama);
+    System.out.println("Umur : " + umur);
+    // komentar satu baris
+    /* komentar
+     lebih dari satu baris */
+    }
+}
