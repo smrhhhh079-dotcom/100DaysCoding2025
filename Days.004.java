d83a7ecd1e24ab2866ba2bde9e39936667fde93a
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-11 17:54:36 +0800

    Days.004.java

---
+++ Days.004.java
@@ -0,0 +1,21 @@
+package Days4.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello \nWorld");
+        System.out.println("Perkenalkan nama saya\bSamra");
+        System.out.println("Tanggal lahir\t 17 April 2006");
+        System.out.println("Saya Kuliah di \"Universitas Sulawesi Barat\"");
+        System.out.println("Saya mengambil prodi \'informatika\' dengan NIM D0225019");
+        
+       // Beberapa Fungsi pengguna Escape Sequence 
+      // \n -> untuk baris baru
+      // \b -> untuk menghapus satu karakter
+      // \t -> untuk memberikan spasi didepan karakter 
+      // \\ -> untuk menampilkan backslash
+      // \" -> untuk menambah tanda kutip pada samping atas kata 
+      // \' -> untuk menambah tanda koma atas 
+    }
+    
+}
