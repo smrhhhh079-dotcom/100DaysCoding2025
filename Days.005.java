c157d5471a0f877b74187305d06b4d187888227c
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-12 20:56:10 +0800

    Days.005.java

---
+++ Days.005.java
@@ -0,0 +1,25 @@
+package Day5;
+
+public class Main {
+    public static void main(String[] args) {
+        //tipe data bilangan bulat
+        byte saudara = 5;
+        int tinggi = 144;
+        short tahun = 2006;
+        long umur = 18;
+        
+        //untuk menampilkan (output)
+        System.out.println("saya mempunyai saudara " + saudara);
+        System.out.println("tinggi badan saya " + tinggi);
+        System.out.println("saya lahir pada tahun " + tahun);
+        System.out.println("umur saya  " + umur);
+        
+        //penjelasan mengenai tipe data
+        /*byte : tipe data yang digunakan untuk data dalam skala kecil.
+        /*short : tipe data dengan jangkauan yang lebih besar dari Byte.
+        /*long : tipe data untuk nilai yang melebihi kapasitas jangkauan tipe data Int,
+         *sehingga memiliki rentang yang jauh lebih tinggi. 
+        /*int : tipe data bilangan bulat yang paling umum.
+         */
+    }   
+}
