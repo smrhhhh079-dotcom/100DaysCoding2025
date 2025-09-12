0461b8fc39ac21b940c7f7121e70de1eea472a74
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-12 21:36:42 +0800

    Days.005.java

--- Days.005.java
+++ Days.005.java
@@ -1,25 +1,24 @@
 package Day5;
 
 public class Main {
-    public static void main(String[] args) {
-        //tipe data bilangan bulat
-        byte saudara = 5;
-        int tinggi = 144;
-        short tahun = 2006;
-        long umur = 18;
+     public static void main(String[] args) {
+        byte a = 10;
+        short b = 200;
+        int c = 5000;
+        long d = 100000L;
+
+        System.out.println("Byte : " + a);
+        System.out.println("Short: " + b);
+        System.out.println("Int  : " + c);
+        System.out.println("Long : " + d);
         
-        //untuk menampilkan (output)
-        System.out.println("saya mempunyai saudara " + saudara);
-        System.out.println("tinggi badan saya " + tinggi);
-        System.out.println("saya lahir pada tahun " + tahun);
-        System.out.println("umur saya  " + umur);
-        
-        //penjelasan mengenai tipe data
-        /*byte : tipe data yang digunakan untuk data dalam skala kecil.
-        /*short : tipe data dengan jangkauan yang lebih besar dari Byte.
-        /*long : tipe data untuk nilai yang melebihi kapasitas jangkauan tipe data Int,
-         *sehingga memiliki rentang yang jauh lebih tinggi. 
-        /*int : tipe data bilangan bulat yang paling umum.
-         */
-    }   
+        //byte a = 10; → tipe data byte (8 bit), hanya bisa menyimpan nilai dari -128 sampai 127.
+
+        //short b = 200; → tipe data short (16 bit), bisa menyimpan nilai dari -32,768 sampai 32,767.
+
+        //int c = 5000; → tipe data int (32 bit), nilai bisa sangat besar, biasanya dipakai default untuk bilangan bulat.
+
+        //long d = 100000L; → tipe data long (64 bit), bisa menyimpan angka yang jauh lebih besar. Perlu ditulis dengan huruf L di akhir supaya dikenali sebagai long.
+
+    }
 }
