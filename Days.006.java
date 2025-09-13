55a1a2adcd658337bfa8bb15ea388c95cc4c8cef
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-13 11:51:47 +0800

    Days.006.java

---
+++ Days.006.java
@@ -0,0 +1,14 @@
+package day6;
+  public class Main {
+       public static void main(String[] args) {
+        
+        //float menyimpan bilangan desimal dengan presisi lebih rendah.
+        //double menyimpan bilangan desimal dengan presisi lebih tinggi daripada float.
+        double tinggi = 144;
+        float berat = 40;
+        
+        //Output
+        System.out.println("Tinggi : " + tinggi + "cm");
+        System.out.println("Berat  : " + berat + "kg");
+    }
+}
