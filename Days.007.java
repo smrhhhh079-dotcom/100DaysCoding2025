6ebccc2031aa053036a80dc95842770e801ceb68
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-14 10:51:03 +0800

    Days.007.java

---
+++ Days.007.java
@@ -0,0 +1,15 @@
+package Day7;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //Tipe data char (1 karakter)
+        char inisial = 'S' ;
+        //Tipe data boolean (menyimpan true/false
+        boolean aktif = true;
+            
+        System.out.println("Inisial Nama: " + inisial);
+        System.out.println("Status Aktif " + aktif);
+    }
+    
+}
