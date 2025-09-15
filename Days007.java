e9ca859cc033f5c111dc8ce17ca4d5d660af3214
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-15 11:12:59 +0800

    Days007.java

---
+++ Days007.java
@@ -0,0 +1,24 @@
+package Day7.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //Tipe data string untuk teks
+        String nama = "Samra";
+        String kelas = "B";
+        String prodi = "Informatika";
+        String hobi = "Membaca";
+        String motivasi = "Setiap error adalah tanda kamu sedang bertumbuh, dan setiap baris kode yang kamu tulis membawa kamu lebih dekat jadi programmer hebat";
+
+        
+        //Output adalah hasil akhir yang ditampilkan oleh program setelah kode dijalankan.
+        System.out.println("Nama  : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("Prodi : " + prodi);
+        System.out.println("Hobi  : " + hobi);
+        System.out.println("Motivasi : " + motivasi);
+    } 
+    
+    
+}
