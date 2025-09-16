1b0475cfeb791833acf10b3e0e4fa4eb4df6d1f8
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-16 21:12:27 +0800

    Days.009.java

---
+++ Days.009.java
@@ -0,0 +1,37 @@
+package Day09.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //Deklarasi variabel
+        int umur;
+        String nama;
+
+        //Inisialisasi variabel
+        umur = 18;
+        nama = "Sam";
+
+        //Deklarasi + inisialisasi langsung
+        double tinggi = 144;
+
+        // Menampilkan nilai awal
+        System.out.println("Nama   : " + nama);
+        System.out.println("Umur   : " + umur);
+        System.out.println("Tinggi : " + tinggi);
+
+        //Update variabel
+        umur = 19;
+        tinggi = 145;
+        nama = "Samra";
+
+        // Menampilkan nilai setelah update
+        System.out.println("\nSetelah update:");
+        System.out.println("Nama   : " + nama);
+        System.out.println("Umur   : " + umur);
+        System.out.println("Tinggi : " + tinggi);
+    
+
+    }
+    
+}
