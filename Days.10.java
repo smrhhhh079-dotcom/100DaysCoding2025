97cb6f9fe76774cb824ff004409e8fbcf56e7ff5
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-17 18:56:58 +0800

    Days.10.java

---
+++ Days.10.java
@@ -0,0 +1,15 @@
+package Day10.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //Final variabel
+        final double tinggi = 144;
+        
+        //Output 
+        System.out.println("Tinggi badan : " + tinggi );
+        
+        //Final variabel adalah variabel yang nilainya tidak bisa diubah lagi setelah diberi nilai (diinisialisasi).
+    }
+    
+}
