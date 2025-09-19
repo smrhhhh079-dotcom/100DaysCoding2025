d84308492d4e8143643357c0860fa1a69e69e22b
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-19 22:00:24 +0800

    Days.12.java

--- Days.12.java
+++ Days.12.java
@@ -1,25 +1,41 @@
-package Day12.java;
+package Day12;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Deklarasi variabel
-        String nama = "Samra";
-        String nim = "D0225019";
-        int umur = 19;
-        char kelas = 'B';
-        String alamat = "Lutang";
-        double tinggiBadan = 144.0;
-        double beratBadan = 40.0;
+        //Membuat objek Scanner
+        Scanner input = new Scanner(System.in);
 
-        // Output biodata
-        System.out.println("Nama         : " + nama);
-        System.out.println("NIM          : " + nim);
-        System.out.println("Umur         : " + umur + " tahun");
-        System.out.println("Kelas        : " + kelas);
-        System.out.println("Alamat       : " + alamat);
-        System.out.println("Tinggi Badan : " + tinggiBadan + " cm");
-        System.out.println("Berat Badan  : " + beratBadan + " kg");
+        // Input data
+        System.out.print("Masukkan Nama: ");
+        String nama = input.nextLine();
+        
+        System.out.print("Masukkan NIM: ");
+        String nim = input.nextLine();
+        
+        System.out.print("Masukkan Umur: ");
+        int umur = input.nextInt();
+        input.nextLine();
 
+        System.out.print("Masukkan Kelas: ");
+        String kelas = input.nextLine();
+
+        System.out.print("Masukkan Alamat: ");
+        String alamat = input.nextLine();
+
+        System.out.print("Masukkan Tinggi Badan (cm): ");
+        double tinggi = input.nextDouble();
+
+        System.out.print("Masukkan Berat Badan (kg): ");
+        double berat = input.nextDouble();
+
+        // Bagian Output data
+        System.out.println("Nama        : " + nama);
+        System.out.println("NIM         : " + nim);
+        System.out.println("Umur        : " + umur + " tahun");
+        System.out.println("Kelas       : " + kelas);
+        System.out.println("Alamat      : " + alamat);
+        System.out.println("Tinggi Badan: " + tinggi + " cm");
+        System.out.println("Berat Badan : " + berat + " kg");
     }
-    
-    
 }
