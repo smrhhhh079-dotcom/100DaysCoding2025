6ed92b1ea06647cf8603963aa7ab49c8c27f7d4c
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-19 19:18:17 +0800

    Days.12.java

---
+++ Days.12.java
@@ -0,0 +1,25 @@
+package Day12.java;
+public class Main {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        String nama = "Samra";
+        String nim = "D0225019";
+        int umur = 19;
+        char kelas = 'B';
+        String alamat = "Lutang";
+        double tinggiBadan = 144.0;
+        double beratBadan = 40.0;
+
+        // Output biodata
+        System.out.println("Nama         : " + nama);
+        System.out.println("NIM          : " + nim);
+        System.out.println("Umur         : " + umur + " tahun");
+        System.out.println("Kelas        : " + kelas);
+        System.out.println("Alamat       : " + alamat);
+        System.out.println("Tinggi Badan : " + tinggiBadan + " cm");
+        System.out.println("Berat Badan  : " + beratBadan + " kg");
+
+    }
+    
+    
+}
