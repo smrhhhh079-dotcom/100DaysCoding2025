4227766c60304ced36396e41aea89591ce586143
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-22 21:15:58 +0800

    Day.15.java

---
+++ Day.15.java
@@ -0,0 +1,22 @@
+package Day15.java;
+public class Main {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 5;
+
+        //Penjumlahan
+        int hasilTambah = a + b;
+
+        //Pengurangan
+        int hasilKurang = a - b;
+
+        //Output
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Hasil Penjumlahan (a + b) = " + hasilTambah);
+        System.out.println("Hasil Pengurangan (a - b) = " + hasilKurang);
+    }
+}
+    
+    
