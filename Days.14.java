a580d94a2f0165fd200669cd94bec668e64106fe
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-22 20:26:24 +0800

    Days.14.java

--- Days.14.java
+++ Days.14.java
@@ -5,15 +5,15 @@ public class Main {
         // Mengubah String menjadi tipe data primitif
         String sam = "29";
         int samra = Integer.parseInt(sam);
-        System.out.println(samra);
+        System.out.println("sam : " + samra);
         
         String saya = "19";
         byte kamu = Byte.parseByte(saya);
-        System.out.println(kamu);
+        System.out.println("saya : " + kamu);
        
         String kakek = "2";
         long nenek = Long.parseLong(kakek);
-        System.out.println(nenek);
+        System.out.println("kakek : " + nenek);
         
         String perempuan = "31";
         short lakilaki = Short.parseShort(perempuan);
@@ -21,19 +21,19 @@ public class Main {
          
         String Paman = "1.2";
         double om = Double.parseDouble(Paman);   
-        System.out.println(om);
+        System.out.println("Paman : " + om);
         
         String meja = "2.3f";
         float kursi = Float.parseFloat(meja);
-        System.out.println(meja);
+        System.out.println("meja : " + kursi);
         
         String kakak="kaka";
         char adek = kakak.charAt(0);
-        System.out.println(kakak);
+        System.out.println("kakak : " + adek);
         
         String papa = "true";
         boolean ibu = Boolean.parseBoolean(papa);
-        System.out.println(papa);
+        System.out.println("papa : " + ibu);
         
         //Mengubah String menjadi tipe data primitif
         //Seperti (String) ke tipe data int,byte,long,short,double,float,char,dan boolean.
