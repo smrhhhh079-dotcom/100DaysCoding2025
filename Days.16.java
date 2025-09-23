f100082868934ac7b03eacc25fee611d871dabbe
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-23 22:03:17 +0800

    Days.16.java

--- Days.16.java
+++ Days.16.java
@@ -1,19 +1,26 @@
 package Day16.java;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int a = 20;
-        int b = 5;
+        Scanner input = new Scanner(System.in);
 
-        //Perkalian
+        // Input
+        System.out.print("Masukkan angka pertama: ");
+        int a = input.nextInt();
+
+        System.out.print("Masukkan angka kedua: ");
+        int b = input.nextInt();
+
+        // Operasi perkalian
         int kali = a * b;
 
-        //Pembagian
+        // Operasi pembagian
         int bagi = a / b;
 
-        //Output
-        System.out.println("Hasil Perkalian: " + kali);
-        System.out.println("Hasil Pembagian: " + bagi);
+        // Output hasil
+        System.out.println("Hasil Perkalian = " + kali);
+        System.out.println("Hasil Pembagian = " + bagi);
+        
     }
 }
-    
     
