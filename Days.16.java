305e128b6a140838da99b52dacbe1db76a2c312f
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-23 21:33:22 +0800

    Days.16.java

---
+++ Days.16.java
@@ -0,0 +1,19 @@
+package Day16.java;
+public class Main {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+
+        //Perkalian
+        int kali = a * b;
+
+        //Pembagian
+        int bagi = a / b;
+
+        //Output
+        System.out.println("Hasil Perkalian: " + kali);
+        System.out.println("Hasil Pembagian: " + bagi);
+    }
+}
+    
+    
