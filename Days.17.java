117297731be06d2da20bc1dff342e0212ec38512
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-24 20:23:58 +0800

    Days.17.java

---
+++ Days.17.java
@@ -0,0 +1,19 @@
+package Day17.java;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = 10;
+        int b = 3;
+
+        int hasil = a % b;
+        
+        //Output
+        System.out.println("Angka pertama: " + a);
+        System.out.println("Angka kedua: " + b);
+        System.out.println("Sisa bagi (a % b): " + hasil);
+    }
+}
+    
+    
