51e7fafb65ae64d406a575c08d13caf5eb5e8934
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-24 21:01:33 +0800

    Days.17.java

--- Days.17.java
+++ Days.17.java
@@ -1,19 +1,15 @@
 package Day17.java;
 import java.util.Scanner;
 public class Main {
-    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int a = 10;
-        int b = 3;
-
-        int hasil = a % b;
         
-        //Output
-        System.out.println("Angka pertama: " + a);
-        System.out.println("Angka kedua: " + b);
+        System.out.print("Masukkan nilai a : ");
+        int a = input.nextInt();
+        System.out.print("Masukkan niali b : ");
+        int b = input.nextInt();
+        
+        int hasil = a % b;
         System.out.println("Sisa bagi (a % b): " + hasil);
     }
 }
-    
-    
