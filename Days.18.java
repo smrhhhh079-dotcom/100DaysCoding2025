12cb802e7daef27d79b3e03d54b2bf1d617af2b9
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-25 22:07:14 +0800

    Days.18.java

---
+++ Days.18.java
@@ -0,0 +1,11 @@
+package Day18.java;
+public class Main {
+    public static void main(String[] args) {
+        //Konversi otomatis (Tipe data kecil ke besar)
+        int angkaInt = 17;
+        float angkaFloat = angkaInt; 
+
+        System.out.println("Int   : " + angkaInt);
+        System.out.println("Float : " + angkaFloat);
+    }
+}
