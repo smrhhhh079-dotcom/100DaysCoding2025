9b4248a2c0dea44c647b746370a90764290ce880
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-26 21:03:03 +0800

    Days.19.java

---
+++ Days.19.java
@@ -0,0 +1,11 @@
+package Day19.java;
+public class Main {
+    public static void main(String[] args) {
+        //Konversi manual/paksa (Dari tipe data besar ke kecil)
+        double d = 17.4;
+        int i = (int) d;
+        
+        System.out.println("double ke int : 17.4 " + i);
+    }
+}
+
