989f93df8af457444747fd79fff53885cf0da8c6
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-09-28 23:12:05 +0800

    Days.21.java

--- Days.21.java
+++ Days.21.java
@@ -5,14 +5,22 @@ public class Main {
     public static void main(String[] args) {
         int a = 20;
         int b = 10;
+        
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        
         System.out.println("sebelum ditukar");
         System.out.println("a = " + a + ",b = " + b);
         
-        int temp = a;
-        a = b;
-        b = temp;
+        int c = 17;
+        int d = 4;
+        
+        int temp = c;
+        c = d;
+        d = temp;
         System.out.println("sesudah ditukar");
-        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("c = " + c + ", d = " + d);
 
     }
 }
