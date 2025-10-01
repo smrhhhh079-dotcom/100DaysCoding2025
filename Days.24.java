e5db6b555a31d5cd78ba9055e507bbe6fd01d71b
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-10-01 19:25:05 +0800

    Days.24.java

---
+++ Days.24.java
@@ -0,0 +1,18 @@
+package Day24;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan jari-jari lingkaran: ");
+        double r = input.nextDouble();
+        
+        //Luas lingkaran
+        double luas = Math.PI * r * r;
+
+        //Output
+        System.out.printf("Luas lingkaran dengan jari-jari :" + luas);
+        
+    }
+}
