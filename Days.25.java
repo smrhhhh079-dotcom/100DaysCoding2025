16a8ef52b54d31f247627d2a4f4b6e69927965f2
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-10-02 22:06:45 +0800

    Days.25.java

--- Days.25.java
+++ Days.25.java
@@ -2,14 +2,13 @@ package Day25;
 public class Main {
     
     public static void main(String[] args) {
-      // Increment (++) dan Decrement (--) 
+        
         int a = 2;
         int b = 3;
-        System.out.println("post increment : a++ " + a);
-        System.out.println("pre increment  : ++a " + a);
-        System.out.println("post decrement : b-- " + b);
-        System.out.println("pre decrement  : --b " + b);
-        
-        
+
+        System.out.println("a++ = " + (a++));
+        System.out.println("++a = " + (++a));
+        System.out.println("b-- = " + (b--));
+        System.out.println("--b = " + (--b));
     }
 }
