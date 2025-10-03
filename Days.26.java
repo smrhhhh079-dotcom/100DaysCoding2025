63099012c78163aaf31c08e8710e50c644bda7b5
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-10-03 21:12:14 +0800

    Days.26.java

---
+++ Days.26.java
@@ -0,0 +1,24 @@
+package Day26;
+public class Main {
+    public static void main(String[] args) {
+        
+        int a = 4,b = 8;
+        
+        //Operator Penugasan
+        a += b;
+        System.out.println(a);
+        
+        a -= b; 
+        System.out.println(a);
+        
+        a *= b;
+        System.out.println(a);
+        
+        a /= b; 
+        System.out.println(a);
+        
+        a %= b; 
+        System.out.println(a);
+    }
+}
+    
