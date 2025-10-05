19555c5bbab1d230fac9c8d6c8f34ed32f2a4393
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-10-05 20:31:19 +0800

    Days.28.java

---
+++ Days.28.java
@@ -0,0 +1,20 @@
+package Day28;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        //Operator perbandingan == dan !=
+        System.out.print("angka1 :");
+        int a = sc.nextInt();
+        System.out.print("angka2 :");
+        int b = sc.nextInt();
+        
+        System.out.println("a == b : " + (a == b ));
+        System.out.println("a !=b : " + (a !=b ));
+        
+    }
+}
+    
+    
