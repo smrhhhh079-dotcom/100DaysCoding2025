b1402b2508a08ede13d738865a4dfe70241246d5
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-10-06 19:42:52 +0800

    Days.29.java

---
+++ Days.29.java
@@ -0,0 +1,20 @@
+package Day29;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan angka pertama: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Masukkan angka kedua: ");
+        double b = sc.nextDouble();
+
+        System.out.println("Apakah a < b? " + (a < b));
+        System.out.println("Apakah a > b? " + (a > b));
+        
+    }
+}
+    
+    
