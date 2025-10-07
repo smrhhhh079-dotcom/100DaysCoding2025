c5dc3c542acb6d589e7ade0554df95a0938f112d
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-10-07 19:26:16 +0800

    Days.30.java

--- Days.30.java
+++ Days.30.java
@@ -4,8 +4,7 @@ public class Main {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-
+        
         System.out.print("Masukkan angka pertama: ");
         int a = sc.nextInt();
 
