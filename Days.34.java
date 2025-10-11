1d4178f9f72a0103fc9151f27a8b9efa6c1c663c
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-10-11 21:37:40 +0800

    Days.34.java

---
+++ Days.34.java
@@ -0,0 +1,24 @@
+package Day34;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukkan nilai a: ");
+        int a = sc.nextInt();
+        
+        System.out.print("Masukkan nilai b: ");
+        int b = sc.nextInt();
+        
+        System.out.print("Masukkan nilai c: ");
+        int c = sc.nextInt();
+        
+        a = a + (b*c);
+        boolean kondisi = (a % 2 == 0 && (a/b > c ))|| (a - b <= 10);
+        
+        System.out.println("Nilai akhir a " + a);
+        System.out.println("Hasil logika " + kondisi);
+    }
+    
+}
