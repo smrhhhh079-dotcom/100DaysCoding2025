99208af1c960c1eb7d46527924bdb62162450bb5
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-10-12 20:36:31 +0800

    Days.35.java

---
+++ Days.35.java
@@ -0,0 +1,21 @@
+package Day35;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan angka: ");
+        int angka = input.nextInt();
+        
+        if (angka % 2 == 0) {
+            System.out.println("Angka genap");
+        } else {
+            System.out.println("Angka ganjil");
+        
+            
+        }
+    }
+}
+    
+    
