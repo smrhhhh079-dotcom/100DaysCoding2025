a44f00f20a3bb66ea75fe26f3acf8b0947e31d10
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-10-14 20:43:32 +0800

    Days.37.java

--- Days.37.java
+++ Days.37.java
@@ -4,15 +4,30 @@ public class Main {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Masukkan angka: ");
-        int angka = sc.nextInt();
-        
-        if(angka % 2 == 0) {
-            System.out.println("Angka yang Anda masukkan adalah genap");
-        }else{
-            System.out.println("Angka yang Anda masukkan adalah ganjil");
+
+        System.out.print("Masukkan Angka : ");
+        int Angka = sc.nextInt();
+
+        if (Angka < 10) {
+            System.out.print("Satuan ");
+        } 
+        else if (Angka >= 10 && Angka < 100) {
+            System.out.print("Puluhan ");
+        } 
+        else if (Angka >= 100 && Angka < 1000) {
+            System.out.print("Ratusan ");
+        } 
+        else {
+            System.out.print("Ribuan ");
+        }
+
+        if (Angka % 2 == 0) {
+            System.out.print("dan Genap\n");
+        } 
+        else {
+            System.out.print("dan Ganjil\n");
         }
     }
-    
 }
+    
+    
