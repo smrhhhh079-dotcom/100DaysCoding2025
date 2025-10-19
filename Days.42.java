4d49691578241a06ff6d4db4908ae3495e8bfd83
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-10-19 20:28:44 +0800

    Days.42.java

--- Days.42.java
+++ Days.42.java
@@ -4,20 +4,59 @@ public class Main {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Nama Pegawai : ");
+        String nama = sc.nextLine();
+        
+        System.out.println("====Pekerjaan ====");
+        System.out.println("1. PNS");
+        System.out.println("2. Freelancer");
+        System.out.println("===========================");
+        System.out.print("Pilih Pekerjaan : ");
+        int pilih = sc.nextInt();
 
-        int GajiPerBulan = 1000000;
-        int GajiLembur = 25000;
+        
+        System.out.print("Penghasilan : ");
+        double penghasilan = sc.nextDouble();
 
-        System.out.print("Masukkan jumlah bulan kerja: ");
-        int Bulan = sc.nextInt();
-
-        System.out.print("Masukkan jumlah jam lembur: ");
-        int JamLembur = sc.nextInt();
-
-        int GajiBersih = (GajiPerBulan * Bulan) + (GajiLembur * JamLembur);
-
-        System.out.println("Gaji Bersih = Rp" + GajiBersih);
+        String pekerjaan = "";
+        double pajak = 0;
+        
+        if (pilih == 1) {
+            pekerjaan = "PNS";
+            if (penghasilan >= 5000000) {
+                pajak = 0.15;
+            } else if (penghasilan >= 3000000) {
+                pajak = 0.10;
+            } else {
+                pajak = 0.0;
+            }
+        } else if (pilih == 2) {
+            pekerjaan = "Freelancer";
+            if (penghasilan >= 5000000) {
+                pajak = 0.08;
+            } else if (penghasilan >= 3000000) {
+                pajak = 0.03;
+            } else {
+                pajak = 0.0;
+            }
+        } else {
+            System.out.println("Pilihan tidak valid!");
+        }
+        
+        double potongan = penghasilan * pajak;
+        double bersih = penghasilan - potongan;
+        
+        System.out.println("============================");
+        System.out.println("Nama Pegawai     : " + nama);
+        System.out.println("Jenis Pekerjaan  : " + pekerjaan);
+        System.out.println("Penghasilan Kotor: Rp" + penghasilan);
+        System.out.println("Pajak Dikenakan  : " + (pajak * 100) + "%");
+        System.out.println("Potongan         : Rp" + potongan);
+        System.out.println("Penghasilan Bersih: Rp" + bersih);
     }
 }
+    
+
     
     
