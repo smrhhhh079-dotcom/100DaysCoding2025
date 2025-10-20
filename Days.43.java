34c26de523ff3e79017342e49b0c69b80412d765
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-10-20 21:49:05 +0800

    Angka yang bisa di bagi 3 dan 5

---
+++ Days.43.java
@@ -0,0 +1,45 @@
+package Day43;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan Angka : ");
+        int angka = input.nextInt();
+
+        if(angka > 0){
+        String digit = "";
+        String jenis = "";
+        String bagi = "";
+        
+        if (angka < 10) {
+            digit = "angka satuan";
+        } else if (angka < 100) {
+            digit = "angka puluhan";
+        } else if (angka < 1000) {
+            digit = "angka ratusan";
+        }
+        
+        if (angka % 2 == 0) {
+            jenis = "genap";
+        } else {
+            jenis = "ganjil";
+        }
+        
+        if (angka % 3 == 0 && angka % 5 == 0) {
+            bagi = "yang bisa di bagi 3 dan 5";
+        } else if (angka % 3 == 0) {
+            bagi = "yang bisa di bagi 3";
+        } else if (angka % 5 == 0) {
+            bagi = "yang bisa di bagi 5";
+        }
+
+        System.out.println(angka + " adalah " + digit + " " + jenis + " " + bagi);
+        } else {
+            System.out.println("Tidak dapat dibagi 3 dan 5");
+        }
+    }
+}
+    
+    
