0c1e9736cd975d9456ea36e091a567664aed8b67
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-10-20 23:04:59 +0800

    Days.43.java

--- Days.43.java
+++ Days.43.java
@@ -8,38 +8,38 @@ public class Main {
         System.out.print("Masukkan Angka : ");
         int angka = input.nextInt();
 
-        if(angka > 0){
-        String digit = "";
-        String jenis = "";
-        String bagi = "";
-        
-        if (angka < 10) {
-            digit = "angka satuan";
-        } else if (angka < 100) {
-            digit = "angka puluhan";
-        } else if (angka < 1000) {
-            digit = "angka ratusan";
-        }
-        
-        if (angka % 2 == 0) {
-            jenis = "genap";
+        if (angka < 0) {
+            System.out.println("Tidak boleh memasukkan angka negatif");
         } else {
-            jenis = "ganjil";
-        }
-        
-        if (angka % 3 == 0 && angka % 5 == 0) {
-            bagi = "yang bisa di bagi 3 dan 5";
-        } else if (angka % 3 == 0) {
-            bagi = "yang bisa di bagi 3";
-        } else if (angka % 5 == 0) {
-            bagi = "yang bisa di bagi 5";
-        }
+            String digit = "";
+            String jenis = "";
+            String bagi = "";
+            
+            if (angka < 10) {
+                digit = "angka satuan";
+            } else if (angka < 100) {
+                digit = "angka puluhan";
+            } else if (angka < 1000) {
+                digit = "angka ratusan";
+            }
+            
+            if (angka % 2 == 0) {
+                jenis = "genap";
+            } else {
+                jenis = "ganjil";
+            }
+            
+            if (angka % 3 == 0 && angka % 5 == 0) {
+                bagi = "yang bisa di bagi 3 dan 5";
+            } else if (angka % 3 == 0) {
+                bagi = "yang bisa di bagi 3";
+            } else if (angka % 5 == 0) {
+                bagi = "yang bisa di bagi 5";
+            } else {
+                bagi = "yang tidak bisa di bagi 3 atau 5";
+            }
 
-        System.out.println(angka + " adalah " + digit + " " + jenis + " " + bagi);
-        } else {
-            System.out.println("Tidak dapat dibagi 3 dan 5");
+            System.out.println(angka + " adalah " + digit + " " + jenis + " " + bagi);
         }
     }
-}
-    
-    
+                       }
