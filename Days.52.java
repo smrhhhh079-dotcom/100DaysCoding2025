d2b2c7d40c2392514e356f7997b650fa99e22617
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-10-29 20:59:01 +0800

    Perulangan While

--- Days.52.java
+++ Days.52.java
@@ -4,28 +4,18 @@ public class Main {
     
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
-        int pilihan = 0;
-        while (pilihan != 4) {
-            System.out.println("=== Menu Pilihan ===");
-            System.out.println("1. Nasi Goreng");
-            System.out.println("2. Mie Ayam");
-            System.out.println("3. Sate Ayam");
-            System.out.println("4. Keluar");
-            System.out.print("Pilih menu (1-4): ");
-            pilihan = input.nextInt();
+        int angka = 1;
+        System.out.print("Masukkan batas angka: ");
+        int batas = input.nextInt();
 
-            if (pilihan == 1) {
-                System.out.println("Kamu memilih Nasi Goreng");
-            } else if (pilihan == 2) {
-                System.out.println("Kamu memilih Mie Ayam");
-            } else if (pilihan == 3) {
-                System.out.println("Kamu memilih Sate Ayam");
-            } else if (pilihan == 4) {
-                System.out.println("Terimakasih sudah beli di sini!");
+        while (angka <= batas) {
+            if (angka % 2 == 0) {
+                System.out.println(angka + " adalah bilangan genap");
             } else {
-                System.out.println("Pilihan tidak valid, coba lagi");
+                System.out.println(angka + " adalah bilangan ganjil");
             }
+            angka++;
         }
+        
     }
 }
