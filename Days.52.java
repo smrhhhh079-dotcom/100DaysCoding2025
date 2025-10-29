e5b521056c66fe216dc508b6b92ee503eacbc8dc
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-10-29 20:45:01 +0800

    Perulangan While

--- Days.52.java
+++ Days.52.java
@@ -4,29 +4,28 @@ public class Main {
     
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int pilihan;
+        
+        int pilihan = 0;
+        while (pilihan != 4) {
+            System.out.println("=== Menu Pilihan ===");
+            System.out.println("1. Nasi Goreng");
+            System.out.println("2. Mie Ayam");
+            System.out.println("3. Sate Ayam");
+            System.out.println("4. Keluar");
+            System.out.print("Pilih menu (1-4): ");
+            pilihan = input.nextInt();
 
-        System.out.println("=== MENU MAKANAN ===");
-        System.out.println("1. Nasi Goreng");
-        System.out.println("2. Mie Ayam");
-        System.out.println("3. Sate Ayam");
-        System.out.println("4. Bakso");
-        System.out.println("5. Keluar");
-        System.out.print("Pilih menu (1-5): ");
-        pilihan = input.nextInt();
-
-        if (pilihan == 1) {
-            System.out.println("Kamu memilih Nasi Goreng");
-        } else if (pilihan == 2) {
-            System.out.println("Kamu memilih Mie Ayam");
-        } else if (pilihan == 3) {
-            System.out.println("Kamu memilih Sate Ayam");
-        } else if (pilihan == 4) {
-            System.out.println("Kamu memilih Bakso");
-        } else if (pilihan == 5) {
-            System.out.println("Terima kasih, program selesai.");
-        } else {
-            System.out.println("Pilihan tidak tersedia.");
+            if (pilihan == 1) {
+                System.out.println("Kamu memilih Nasi Goreng");
+            } else if (pilihan == 2) {
+                System.out.println("Kamu memilih Mie Ayam");
+            } else if (pilihan == 3) {
+                System.out.println("Kamu memilih Sate Ayam");
+            } else if (pilihan == 4) {
+                System.out.println("Terimakasih sudah beli di sini!");
+            } else {
+                System.out.println("Pilihan tidak valid, coba lagi");
+            }
         }
     }
 }
