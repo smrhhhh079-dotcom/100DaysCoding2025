ec7102298048c8a1e9d78d747a680c96760f2d43
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-11-05 19:57:30 +0800

    Days.59.java

--- Days.59.java
+++ Days.59.java
@@ -5,21 +5,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Masukkan nilai N: ");
-        int N = input.nextInt();
+        int n = input.nextInt();
 
-        System.out.println("Angka ganjil dari 1 sampai " + N + ":");
-        for (int i = 1; i <= N; i++) {
-            if (i % 2 != 0) {
-                System.out.print(i + " ");
-            }
+        System.out.println("Angka genap: ");
+        for (int i = 2; i <= n; i+=2) {
+            System.out.print(i + " ");
+            
         }
 
-        System.out.println("\nAngka genap dari " + N + " sampai 1:");
-        for (int i = N; i >= 1; i--) {
-            if (i % 2 == 0) {
-                System.out.print(i + " ");
-            }
+        System.out.println("Angka ganjil: ");
+        for (int i = 1; i <= n; i+=2) {
+            System.out.print(i + " ");
+            
         }
     }
 }
+
     
