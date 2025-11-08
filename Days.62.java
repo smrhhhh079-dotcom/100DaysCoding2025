4dfd3ca04e3c34543dc607cddff21d201aa67b4a
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-11-08 21:13:17 +0800

    Days.62.java

---
+++ Days.62.java
@@ -0,0 +1,22 @@
+package Day62;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan nilai N: ");
+        int n = input.nextInt();
+        
+        int jumlah = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            jumlah += i; 
+        }
+        
+        System.out.println("Jumlah dari 1 sampai " + n + " adalah: " + jumlah);
+    }
+}
+    
+    
