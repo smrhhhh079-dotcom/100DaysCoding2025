20064fbcf162365793bb8c0d827286f0e91ea5c9
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-11-12 20:20:59 +0800

    Membuat pola vertikal

---
+++ Days.66.java
@@ -0,0 +1,15 @@
+package Day66;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //Membuat Pola Vertikal
+        System.out.print("Masukkan jumlah baris: ");
+        int n = input.nextInt(); 
+        
+        for (int i = 1; i <= n; i++) {
+            System.out.println("*");
+        }
+    }
+}
