af32b619392e4c30ce7156e6f20104bf2b55adce
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-11-22 21:20:59 +0800

    Days.76.java

--- Days.76.java
+++ Days.76.java
@@ -1,28 +1,41 @@
-package days76;
-
-public class Days76 {
-
+package Day76;
+import java.util.Scanner;
+public class Main {
+    
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-        String s1 = "Java";
-        String s2 = "java";
-        String s3 = "";
-        String s4 = "Belajar Java Programming";
+        System.out.print("Masukkan teks pertama : ");
+        String teks1 = input.nextLine();
 
-        System.out.println("=== equals() ===");
-        System.out.println("s1.equals(s2) : " + s1.equals(s2));
-        System.out.println("s1.equals(\"Java\") : " + s1.equals("Java"));
+        System.out.print("Masukkan teks kedua   : ");
+        String teks2 = input.nextLine();
 
-        System.out.println("\n=== equalsIgnoreCase() ===");
-        System.out.println("s1.equalsIgnoreCase(s2) : " + s1.equalsIgnoreCase(s2));
-
-        System.out.println("\n=== contains() ===");
-        System.out.println("s4.contains(\"Java\") : " + s4.contains("Java"));
-        System.out.println("s4.contains(\"Python\") : " + s4.contains("Python"));
-
-
-        System.out.println("\n=== isEmpty() ===");
-        System.out.println("s3.isEmpty() : " + s3.isEmpty());
-        System.out.println("s1.isEmpty() : " + s1.isEmpty());
+        
+        if (teks1.isEmpty()) {
+            System.out.println("Teks pertama KOSONG");
+        } else {
+            System.out.println("Teks pertama TIDAK kosong");
+        }
+        
+        if (teks1.equals(teks2)) {
+            System.out.println("Kedua teks SAMA (equals)");
+        } else {
+            System.out.println("Kedua teks BERBEDA (equals)");
+        }
+        
+        if (teks1.equalsIgnoreCase(teks2)) {
+            System.out.println("Kedua teks sama TANPA memperhatikan huruf besar-kecil (equalsIgnoreCase)");
+        } else {
+            System.out.println("Kedua teks berbeda (equalsIgnoreCase)");
+        }
+        
+        if (teks1.contains(teks2)) {
+            System.out.println("Teks pertama MENGANDUNG teks kedua (contains)");
+        } else {
+            System.out.println("Teks pertama TIDAK mengandung teks kedua (contains)");
+        }
     }
 }
+    
+    
