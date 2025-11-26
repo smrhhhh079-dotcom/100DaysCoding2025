5c074cc93bad2f1ef45ff763b8ce04250b39db6b
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-11-26 21:28:30 +0800

    Days.80.java

---
+++ Days.80.java
@@ -0,0 +1,14 @@
+package Day80;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String[] data = {"Data1", "Data2", "Data3", "Data4"};
+
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i]);
+        }
+    }
+}
+    
+    
