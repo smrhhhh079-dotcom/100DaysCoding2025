6a82d518c72590b86af9dc96ed9fab7941ed6fe7
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-11-27 21:59:15 +0800

    Days.81.java

---
+++ Days.81.java
@@ -0,0 +1,23 @@
+package Day81;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        String[] buah = new String [5];
+        buah[0] = "Mangga";
+        buah[1] = "Apel";
+        buah[2] = "Langsat";
+        buah[3] = "Durian";
+        buah[4] = "Rambutan";
+        
+        System.out.println("=== Elemen-elemen Array Buah ===");
+        System.out.println("Elemen 1: " + buah[0]);
+        System.out.println("Elemen 2: " + buah[1]);
+        System.out.println("Elemen 3: " + buah[2]);
+        System.out.println("Elemen 4: " + buah[3]);
+        System.out.println("Elemen 5: " + buah[4]);
+    }
+    
+}
+    
