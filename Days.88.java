1eb9ad2a1e54924a452a843d62c1d1a9c8ca7fd3
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-12-04 21:40:49 +0800

    Days.88.java

---
+++ Days.88.java
@@ -0,0 +1,13 @@
+package Day88;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int[] angka = {10, 20, 30, 40, 50};
+
+        System.out.println("Isi array:");
+        for (int x : angka) {
+            System.out.println(x);
+        }
+    }
+}
