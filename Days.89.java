13521f8612e187593828aca6f3c310cacc84ac68
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-12-05 21:54:45 +0800

    Days.89.java

---
+++ Days.89.java
@@ -0,0 +1,12 @@
+package Day89;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        nama();
+    }
+    public static void nama(){
+        System.out.println("Samra");
+    }
+    
+}
