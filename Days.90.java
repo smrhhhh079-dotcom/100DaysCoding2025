dd7428c2cf74192b521c78d804355a7c72faaad8
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-12-06 21:27:28 +0800

    Days.90.java

---
+++ Days.90.java
@@ -0,0 +1,12 @@
+package Day90;
+
+public class Main {
+    public static int tambah(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int hasil = tambah(5, 3);
+        System.out.println("Hasil: " + hasil); // Output: Hasil: 8
+    }
+}
