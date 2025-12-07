af0f9bedf6e056c3515c554bad9792c29bdf6ce9
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-12-07 20:45:44 +0800

    Days.91.java

---
+++ Days.91.java
@@ -0,0 +1,13 @@
+package Day91;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        sapa("Samra");
+        
+    }
+    public static void sapa(String nama ) {
+        System.out.println("Nama saya " + nama);
+    }
+    
+}
