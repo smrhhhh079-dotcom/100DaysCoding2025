7fbb64261d82c6f1d631e292a8ba3e1deab7f10a
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-12-10 21:09:26 +0800

    Days.94.java

---
+++ Days.94.java
@@ -0,0 +1,18 @@
+package Day94;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        cetakHasil(7, 8);
+        cetakHasil(11, 2);
+        
+        
+    }
+    public static void cetakHasil(int a, int b){
+        int hasil = a + b;
+        System.out.println("Hasil Penjumlahan: " + hasil);
+    }
+
+}
+    
+    
