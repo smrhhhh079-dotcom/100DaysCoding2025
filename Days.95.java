f8510b21333f9ae63807106521b24f2ff0d3d0c8
smrhhhh079-dotcom <smrhhhh079@gmail.com>
2025-12-11 21:42:16 +0800

    Days.95.java

---
+++ Days.95.java
@@ -0,0 +1,24 @@
+package Day95;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Nama("Samra");
+        Umur(19);
+        Tinggi(145);
+    }
+    public static void Nama(String nama) {
+        System.out.println("Nama saya : " + nama);
+    }
+
+    public static void Umur(int umur) {
+        System.out.println("Umur saya : " + umur);
+    }
+
+    public static void Tinggi(double tinggi) {
+        System.out.println("Tinggi saya : " + tinggi);
+    }
+    
+}
+    
+    
